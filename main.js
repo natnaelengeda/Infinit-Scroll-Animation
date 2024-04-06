3676edfc0d2d0461f4e2404bcf2c29587b4fceec
natnaelengeda <nattynengeda@gmail.com>
2024-04-06 09:57:57 +0300

    Refactor animation and add duplicate items in scroller

--- main.js
+++ main.js
@@ -1,9 +1,5 @@
 const scrollers = document.querySelectorAll(".scroller")
 
-if (!window.matchMedia("(prefers-reduced-motion: reducs").matches) {
-  addAnimation()
-}
-
 function addAnimation() {
   scrollers.forEach(scroller => {
     scroller.setAttribute("data-animated", true)
